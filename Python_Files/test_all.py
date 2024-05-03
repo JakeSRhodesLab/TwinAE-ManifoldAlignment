d9cf4812274f589221a776edafdc4a81c06cc90e
Jake Rhodes <jakerhodes8@gmail.com>
2024-05-03 15:31:44 -0600

    add more results

--- Python_Files/test_all.py
+++ Python_Files/test_all.py
@@ -2,8 +2,15 @@
 
 import test_manifold_algorithms as tma
 
+csv_files = ['audiology.csv', 'parkinsons.csv', 'heart_failure.csv', 'ecoli_5.csv',
+            'ionosphere.csv', 'segmentation.csv', 'seeds.csv', 'audiology.csv', 'flare1.csv', 'heart_disease.csv', 'crx.csv', 'hill_valley.csv',
+            'breast_cancer.csv', 'diabetes.csv', 'titanic.csv', 'car.csv', 'chess.csv', 'tic-tac-toe.csv', 'optdigits.csv', 'waveform.csv',
+            'segmentation.csv']
+
+
+
 """Testing All functions"""
-class_instances = tma.run_all_tests(csv_files = ['iris.csv', 'balance_scale.csv', 'glass.csv'], test_random = 2, #General function arguments
+class_instances = tma.run_all_tests(csv_files = csv_files, test_random = 2, #General function arguments
                                 split = "random", verbose = 2, percent_of_anchors = [0.05, 0.1, 0.15, 0.2, 0.3], #Init Key arguments
                                 run_DIG = True, page_ranks = ("None", "off-diagonal", "full"), predict = True, #DIG key arguments
                                 run_DTA = True,
