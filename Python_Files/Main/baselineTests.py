388684d2d24134ef9c8e0b21fd469fe848300dc1
rustadadam <rustadadam@gmail.com>
2024-12-28 15:04:23 -0700

    Finished -- in middle of testing

--- Python_Files/Main/baselineTests.py
+++ Python_Files/Main/baselineTests.py
@@ -16,19 +16,35 @@ from Helpers.Pipeline_Helpers import get_RF_score, get_embedding_scores
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from joblib import Parallel, delayed
 from Helpers.utils import dataprep
+from tqdm import tqdm
+from tqdm_joblib import tqdm_joblib
+print("Imports completed.\n-------------------------------------------------\n\n")
+
+
+#Determine to run classification or regression
+run_regression = None
+
+while run_regression is None:
+    user_input = input("Type 'True' to run regression or 'False' to do classification: ")
+    run_regression = True if user_input == "True" else False if user_input == "False" else None
+
+print("Running...\n")
+
 
 # Data sets below.
-classification_csv = [
-    "zoo.csv", "hepatitis.csv", "iris.csv", "audiology.csv", "parkinsons.csv", "seeds.csv", "segmentation.csv", "glass.csv", "heart_disease.csv", "heart_failure.csv", "flare1.csv", "ecoli_5.csv", "ionosphere.csv",
-    "Cancer_Data.csv", "hill_valley.csv", "balance_scale.csv", "S-curve", "blobs", 'winequality-red.csv', 'car.csv', "crx.csv", "breast_cancer.csv", "titanic.csv", "diabetes.csv", "tic-tac-toe.csv",
-    'Medicaldataset.csv', "water_potability.csv", 'treeData.csv', "optdigits.csv", "waveform.csv", "chess.csv", "artificial_tree.csv"
-    ]
-
-regression_csv = [
-    "EnergyEfficiency.csv", "Hydrodynamics.csv", "OpticalNetwork.csv","AirfoilSelfNoise.csv","AutoMPG.csv","ComputerHardware.csv","CommunityCrime.csv",
-    "ConcreteSlumpTest.csv", "FacebookMetrics.csv", "Parkinsons.csv", "IstanbulStock.csv", "Automobile.csv", "ConcreteCompressiveStrength.csv", "SML2010.csv"
-    ]
+if not run_regression:  
+    csv_files = [
+        "zoo.csv", "hepatitis.csv", "iris.csv", "audiology.csv", "parkinsons.csv", "seeds.csv", "segmentation.csv", "glass.csv", "heart_disease.csv", "heart_failure.csv", "flare1.csv", "ecoli_5.csv", "ionosphere.csv",
+        "Cancer_Data.csv", "hill_valley.csv", "balance_scale.csv", "S-curve", "blobs", 'winequality-red.csv', 'car.csv', "crx.csv", "breast_cancer.csv", "titanic.csv", "diabetes.csv", "tic-tac-toe.csv",
+        'Medicaldataset.csv', "water_potability.csv", 'treeData.csv', "optdigits.csv", "waveform.csv", "chess.csv", "artificial_tree.csv"
+        ]
+else: 
+    csv_files = [
+        "EnergyEfficiency.csv", "Hydrodynamics.csv", "OpticalNetwork.csv","AirfoilSelfNoise.csv","AutoMPG.csv","ComputerHardware.csv","CommunityCrime.csv",
+        "ConcreteSlumpTest.csv", "FacebookMetrics.csv", "Parkinsons.csv", "IstanbulStock.csv", "Automobile.csv", "ConcreteCompressiveStrength.csv", "SML2010.csv"
+        ]
 
 
 # Data Prep Function
@@ -39,9 +55,9 @@ def prep_data_file(csv_file, seed):
     """
 
     #Find whether its a classification or regression problem
-    try:
+    if not run_regression:
         df = pd.read_csv("/yunity/arusty/Graph-Manifold-Alignment/Resources/Classification_CSV/" + csv_file)
-    except:
+    else:
         df = pd.read_csv("/yunity/arusty/Graph-Manifold-Alignment/Resources/Regression_CSV/" + csv_file)
 
     #Split data into features and labels. This also normalizes.
@@ -63,8 +79,8 @@ def prep_data_file(csv_file, seed):
 
 # Evaluation function
 def get_results(csv_file, seed):
-    knn_score, rf_score, knn_rmse, rf_rmse = get_embedding_scores(**prep_data_file(csv_file, seed))
-    rf_oob = get_RF_score(**prep_data_file(csv_file, seed))
+    knn_score, rf_score, knn_rmse, rf_rmse = get_embedding_scores(*prep_data_file(csv_file, seed))
+    rf_oob = get_RF_score(*prep_data_file(csv_file, seed))
 
     #Return it as a dictionary so we can make a Pandas table easier later
     return {"csv_file" : csv_file, "Method": "Pipeline Baseline", 
@@ -74,3 +90,19 @@ def get_results(csv_file, seed):
             "Nearest Neighbor (F1 score or RMSE)": knn_rmse,
             "Random Forest (F1 score or RMSE)": rf_rmse
             }
+
+
+csv_seed_list = []
+for seed in [1738, 5271, 9209, 1316, 42]: 
+    for csv in csv_files: #CHANGE THIS FOR REGRESSION OR NOT
+        csv_seed_list.append((csv, seed))
+
+# Get the results and show progress
+with tqdm_joblib(tqdm(desc="Processing tasks", total=len(csv_seed_list))) as progress_bar:
+    results = Parallel(n_jobs=10)(delayed(get_results)(csv, seed) for csv, seed in csv_seed_list)
+
+#Write Pandas dataframe
+if run_regression:
+    pd.DataFrame(results).to_csv("/yunity/arusty/Graph-Manifold-Alignment/Results/ManifoldData/PipelineBasline.csv")
+else:
+    pd.DataFrame(results).to_csv("/yunity/arusty/Graph-Manifold-Alignment/Results/RegressionData/PipelineBaseline.csv")
