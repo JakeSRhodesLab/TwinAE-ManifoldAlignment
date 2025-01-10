b677551f3ad35dbffecd342f3b6adad00bf3ace7
rustadadam <rustadadam@gmail.com>
2025-01-10 08:48:30 -0700

    Outline begging functions

---
+++ Python_Files/Helpers/Mantels_Helpers.py
@@ -0,0 +1,5 @@
+# Create function to Extract best fit information from the results
+
+# Create function to create the embeddings (One with excluded test points) from Mash or SPUD
+
+# Create the GRAE model evaluation
