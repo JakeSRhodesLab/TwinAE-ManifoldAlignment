3bd8e632ce84f312d26070708684656d239f0869
rustadadam <rustadadam@gmail.com>
2025-01-23 09:42:04 -0700

    Adjust parallel processing in Mantel.py to use dynamic job allocation

--- Python_Files/Main/Mantel.py
+++ Python_Files/Main/Mantel.py
@@ -20,12 +20,11 @@ random.shuffle(tasks)
 print("Tasks created.")
 
 with tqdm_joblib(tqdm(total=len(tasks))): #This includes a progress bar :)
-    with Parallel(n_jobs=1) as parallel:
+    with Parallel(n_jobs=-5) as parallel:
         parallel(
             delayed(mantel_test)(*task)
             for task in tasks
         )
-
 
 #NOTE: You can command click the stub to access where its defined. 
 
