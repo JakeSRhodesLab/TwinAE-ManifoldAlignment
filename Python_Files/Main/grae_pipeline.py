0710a08f1e19cf9151b655367e2b00aece23ef78
rustadadam <rustadadam@gmail.com>
2025-03-03 11:17:48 -0700

    Update task creation in grae_pipeline to include MSE and adjust parallelization settings

--- Python_Files/Main/grae_pipeline.py
+++ Python_Files/Main/grae_pipeline.py
@@ -1,5 +1,5 @@
 #Imports
-from Helpers.grae_pipeline_helpers import extract_all_files, GRAE_tests, create_tasks_for_parrelization
+from Helpers.grae_pipeline_helpers import extract_all_files, GRAE_tests, create_tasks_for_parrelization, create_tasks_for_DTA_MAGAN_MASH, create_tasks_MSE
 from joblib import Parallel, delayed
 from tqdm_joblib import tqdm_joblib
 from tqdm import tqdm
@@ -11,14 +11,16 @@ print("Imports complete.")
 df = extract_all_files()
 
 #Create the tasks
-tasks = create_tasks_for_parrelization(df)
+### tasks = create_tasks_for_parrelization(df)
+### tasks = create_tasks_for_DTA_MAGAN_MASH(df)
+tasks = create_tasks_MSE(df)
 
 random.shuffle(tasks)
 
 print("Tasks created.")
 
 with tqdm_joblib(tqdm(total=len(tasks))): #This includes a progress bar :)
-    with Parallel(n_jobs=-10) as parallel:
+    with Parallel(n_jobs=-15) as parallel:
         parallel(
             delayed(GRAE_tests)(*task)
             for task in tasks
